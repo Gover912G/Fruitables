12bbdfce13a1ed84996586aef2739521b91e99f2
Gover912G <ngigipaul912@gmail.com>
2024-06-09 17:13:46 +0300

    navigation links done

--- fruitables/grocery/urls.py
+++ fruitables/grocery/urls.py
@@ -5,4 +5,11 @@ from . import views
 app_name = "grocery"
 urlpatterns = [
     path('', views.home, name="index"),
+    path('cart', views.cart, name="cart"),
+    path('checkout', views.checkout, name="checkout"),
+    path('contact', views.contact, name="contact"),
+    path('shop', views.shop, name="shop"),
+    path('shop-detail', views.shopDetail, name="shop-detail"),
+    path('testimonial', views.testimonial, name="testimonial"),
+
 ]
--- fruitables/grocery/views.py
+++ fruitables/grocery/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html', {'nav':'home'})
+
+def cart(request):
+    return render(request, 'cart.html', {'nav':'cart'})
+
+def checkout(request):
+    return render(request, 'checkout.html', {'nav':'checkout'})
+
+def contact(request):
+    return render(request, 'contact.html', {'nav':'contact'})
+
+def shop(request):
+    return render(request, 'shop.html', {'nav':'shop'})
+
+def shopDetail(request):
+    return render(request, 'shop-detail.html', {'nav':'shop-detail'})
+
+def testimonial(request):
+    return render(request, 'testimonial.html', {'nav':'testimonial'})
