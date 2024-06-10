11c35edb78ce85fda9e1ce1a681310b29bc2c733
Gover912G <ngigipaul912@gmail.com>
2024-06-10 23:57:47 +0300

    models created and migrated

--- fruitables/grocery/admin.py
+++ fruitables/grocery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import hero,vegetable,Fruit
+
 # Register your models here.
+admin.site.register(hero)
+admin.site.register(vegetable)
+admin.site.register(Fruit)
--- fruitables/grocery/models.py
+++ fruitables/grocery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class hero(models.Model):
+    img_fruits = models.ImageField(upload_to='fruits')
+    img_vegetables = models.ImageField(upload_to='vesitables')
+
+
+class vegetable(models.Model):
+    name = models.CharField(blank=False, max_length=100)
+    text = models.TextField(blank=False, default='text')
+    price = models.DecimalField(max_digits=6, decimal_places=2, )
+    image = models.ImageField(upload_to='vesitables')
+
+
+class Fruit(models.Model):
+    name = models.CharField(blank=False, max_length=100)
+    text = models.TextField(blank=False, default='text')
+    price = models.DecimalField(max_digits=6, decimal_places=2, )
+    image = models.ImageField(upload_to='fruits')
