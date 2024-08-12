5d797a0dca0a98eab9150b077753248c89d377d3
Gover912G <ngigipaul912@gmail.com>
2024-08-13 00:29:00 +0300

    partial login auth

--- fruitables/urls.py
+++ fruitables/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('grocery.urls')),
+    path('users', include('django.contrib.auth.urls.')),
     path('users', include('users.urls')),
 ]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
